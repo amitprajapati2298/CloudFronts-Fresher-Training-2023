e4a5737478cd85f99a2142e16656957eeb3a4a90
Amit Prajapati <amitgprajapati1998@gmail.com>
2023-10-27 09:01:12 +0530

    Added the Boostrap CSS

--- Mahabharat Game/js/script.js
+++ Mahabharat Game/js/script.js
@@ -6,7 +6,7 @@ var mahabharatGame = {
         playerCount:0,
         skillCount:0,
         constants:{
-            player:"Playacha ers",
+            player:"Player",
             skill:"skill"
         }
     },
@@ -14,25 +14,36 @@ var mahabharatGame = {
         var userinput = prompt("Please provide your Player Details in <first>:<lastname>");
         var firstname = userinput.split(":")[0];
         var lastname = userinput.split(":")[1];
-        var playerObject = {};
-        playerObject["id"] = mahabharatGame.memory.playerCount++;
-        playerObject["firstname"] = firstname;
-        playerObject["lastname"] = lastname;
-        gameScreen.addContentToConsole(`${firstname} ${lastname}`, mahabharatGame.memory.constants.player);
-        mahabharatGame.memory.players.push(playerObject);
-        console.log(mahabharatGame.memory);
+        if(firstname !== "" && firstname !== undefined && lastname !== "" && lastname !== undefined){
+            var playerObject = {};
+            playerObject["id"] = mahabharatGame.memory.playerCount++;
+            playerObject["firstname"] = firstname;
+            playerObject["lastname"] = lastname;
+            gameScreen.addContentToConsole(`${firstname} ${lastname}`, mahabharatGame.memory.constants.player);
+            mahabharatGame.memory.players.push(playerObject);
+            console.log(mahabharatGame.memory);
+        }else{
+            alert("Please enter the Correct First Name and Last name")
+        }
+        
     },
     addSkill: function () {
         var userinput = prompt("Please provide your skills in <skillName>:<maxLevel>");
         var skilname = userinput.split(":")[0];
-        var maxlevel = parseInt(userinput.split(":")[1]);
-        var skillObject = {};
-        skillObject["id"] = mahabharatGame.memory.skillCount++;
-        skillObject["skilname"] = skilname;
-        skillObject["maxlevel"] = maxlevel;
-        gameScreen.addContentToConsole(`${skilname} ${maxlevel}`, mahabharatGame.memory.constants.skill);
-        mahabharatGame.memory.skills.push(skillObject);
-        console.log(mahabharatGame.memory);
+        var maxLevelString = userinput.split(":")[1];
+        if(skilname !== "" && maxLevelString !== "" && skilname !== undefined && maxLevelString !== undefined){
+            var maxlevel = parseInt(maxLevelString);
+            var skillObject = {};
+            skillObject["id"] = mahabharatGame.memory.skillCount++;
+            skillObject["skilname"] = skilname;
+            skillObject["maxlevel"] = maxlevel;
+            gameScreen.addContentToConsole(`${skilname} ${maxlevel}`, mahabharatGame.memory.constants.skill);
+            mahabharatGame.memory.skills.push(skillObject);
+            console.log(mahabharatGame.memory);
+        }else{
+            alert("Please enter the Correct Skillname and Max Levels")
+        }
+        
     }
     
 }
@@ -41,10 +52,19 @@ var gameScreen= {
     addContentToConsole: function (message, messageType){
         if(messageType === mahabharatGame.memory.constants.player){
             var existingText = document.getElementById("playerList").innerHTML ;
-            document.getElementById("playerList").innerHTML = existingText +"<br>" + message;
+            if(existingText !== ""){
+                document.getElementById("playerList").innerHTML = existingText +"<br>" + message;
+            }else{
+                document.getElementById("playerList").innerHTML = message;
+            }
+            
         }else if(messageType === mahabharatGame.memory.constants.skill){
             var existingText = document.getElementById("skillList").innerHTML ;
-            document.getElementById("skillList").innerHTML = existingText +"<br>" + message;
+            if(existingText !== ""){
+                document.getElementById("skillList").innerHTML = existingText +"<br>" + message;
+            }else{
+                document.getElementById("skillList").innerHTML = message;
+            }
         }
         
     },
